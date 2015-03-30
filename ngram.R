a3e864cd26d47ecea01f57f3d03701e883fdfe75
Melissa Tan <offwinter@gmail.com>
2015-03-30 10:20:53 +0800

    fixed ngram tokenizer delimiters

--- ngram.R
+++ ngram.R
@@ -19,7 +19,8 @@ SampleTxt <- function(infile, outfile, seed, inlines, percent, readmode) {
   # for each line, flip a coin to decide whether to put it in sample
   set.seed(seed)
   in.sample <- rbinom(n=inlines, size=1, prob=percent)
-  i <- 0
+  i <- 0  # have to use for-loop, not while-loop, bec of in.sample array
+  num.out <- 0
   for (i in 1:(inlines+1)) {
     # read in one line at a time
     currLine <- readLines(conn.in, n=1, encoding="UTF-8", skipNul=TRUE)
@@ -41,24 +42,36 @@ SampleTxt <- function(infile, outfile, seed, inlines, percent, readmode) {
 datalist <- c("../final/en_US/en_US.blogs.txt",
               "../final/en_US/en_US.news.txt",
               "../final/en_US/en_US.twitter.txt")
-mypercent <- 0.05
+mypercent <- 0.1
 myseed <- 60637
-# got the below using wc -l, hardcoded bec lazy
-blog.sample.numlines <- 45506
-news.sample.numlines <- 51087
-twit.sample.numlines <- 119260
-
+pwd <- getwd()
+setwd("../final/en_US")
+blog.numlines <- as.numeric(gsub('[^0-9]', '', system("wc -l en_US.blogs.txt", intern=TRUE)))
+news.numlines <- as.numeric(gsub('[^0-9]', '', system("wc -l en_US.news.txt", intern=TRUE)))
+twit.numlines <- as.numeric(gsub('[^0-9]', '', system("wc -l en_US.twitter.txt", intern=TRUE)))
+setwd(pwd)
 # files should already exist in dir. if they don't, make them
-if (!file.exists("./blog.sample5.txt")) {
-  blog.sample.numlines <- SampleTxt(datalist[1], "blog.sample5.txt", myseed, blog.numlines, mypercent, "r")
+if (!file.exists("./blog.sample.txt")) {
+  blog.sample.numlines <- SampleTxt(datalist[1], "blog.sample.txt",
+                                    myseed, blog.numlines, mypercent, "r")
 }
-if (!file.exists("./news.sample5.txt")) {
+if (!file.exists("./news.sample10.txt")) {
   # must use readmode "rb" here, otherwise it breaks on a special char
-  news.sample.numlines <- SampleTxt(datalist[2], "news.sample5.txt", myseed, news.numlines, mypercent, "rb")
+  news.sample.numlines <- SampleTxt(datalist[2], "news.sample.txt",
+                                    myseed, news.numlines, mypercent, "rb")
 }
-if (!file.exists("./twit.sample5.txt")) {
-  twit.sample.numlines <- SampleTxt(datalist[3], "twit.sample5.txt", myseed, twit.numlines, mypercent, "r")
+if (!file.exists("./twit.sample.txt")) {
+  twit.sample.numlines <- SampleTxt(datalist[3], "twit.sample.txt",
+                                    myseed, twit.numlines, mypercent, "r")
 }
+
+# get the number of lines in sample, using wc -l
+blog.sample.numlines <- as.numeric(gsub('[^0-9]', '',
+                          system("wc -l blog.sample.txt", intern=TRUE)))
+news.sample.numlines <- as.numeric(gsub('[^0-9]', '',
+                          system("wc -l news.sample.txt", intern=TRUE)))
+twit.sample.numlines <- as.numeric(gsub('[^0-9]', '',
+                          system("wc -l twit.sample.txt", intern=TRUE)))
 
 ## Function to partition subsample txt file
 PartitionTxt <- function(infile, outfiles, seed, inlines, trainpct, readmode) {
@@ -114,18 +127,18 @@ PartitionTxt <- function(infile, outfiles, seed, inlines, trainpct, readmode) {
 myseed <- 773; mytrain <- 60
 # Create train, valid, test sets, in UTF-8 encoding
 if (!file.exists("./blog.train.txt")) {
-  PartitionTxt("./blog.sample5.txt",
+  PartitionTxt("./blog.sample.txt",
                  c("blog.train.txt","blog.valid.txt","blog.test.txt"),
                  myseed, blog.sample.numlines, mytrain, "r")
 }
 if (!file.exists("./news.train.txt")) {
   # must use readmode "rb" here, otherwise it breaks on a special char
-  PartitionTxt("news.sample5.txt",
+  PartitionTxt("news.sample.txt",
               c("news.train.txt","news.valid.txt","news.test.txt"),
               myseed, news.sample.numlines, mytrain, "rb")
 }
 if (!file.exists("./twit.train.txt")) {
-  PartitionTxt("twit.sample5.txt",
+  PartitionTxt("twit.sample.txt",
               c("twit.train.txt","twit.valid.txt","twit.test.txt"),
               myseed, twit.sample.numlines, mytrain, "r")
 }
@@ -134,7 +147,7 @@ if (!file.exists("./twit.train.txt")) {
 removeURL <- function(x) {
   gsub("http.*?( |$)", "", x)
 }
-removeSpecial <- function(x) {
+convertSpecial <- function(x) {
   # replace any <U+0092> with single straight quote, remove all other <>
   x <- gsub("<U.0092>","'",x)  # actually unnecessary, but just in case
   gsub("<.+?>"," ",x)
@@ -143,42 +156,40 @@ myRemoveNumbers <- function(x) {
   # remove any word containing numbers
   gsub("\\S*[0-9]+\\S*", " ", x)
 }
-mySingleQuote <- function(x) {
-  # convert smart single quotes to straight single quotes
-  gsub("[\x82\x91\x92]", "'", x)  # ANSI version, not Unicode version
+removeProfanity <- function(x) {
+  # remove any string that contains *
+  gsub("\\S*[*]+\\S*", " ", x)
 }
-myRemovePunctuation <- function(x) {
+myRemovePunct <- function(x) {
   # custom function to remove most punctuation
   # replace everything that isn't alphanumeric, space, ', -, *
-  gsub("[^[:alnum:][:space:]'-*]", " ", x)
+  gsub("[^[:alnum:][:space:]'*-]", " ", x)
 }
 myDashApos <- function(x) {
   # deal with dashes, apostrophes, asterisks within words
   x <- gsub("--+", " ", x)
-  gsub("(\\w['-*]\\w)|[[:punct:]]", "\\1", x, perl=TRUE)
+  # preserve intra-word dashes, apostrophes, remove all else
+  gsub("(\\w['*-]\\w)|[[:punct:]]", "\\1", x)
 }
 trim <- function(x) {
   # trim leading and trailing whitespace
   gsub("^\\s+|\\s+$", "", x)
 }
-CleanCorpus <- function(my.corpus) {  # input should be a Corpus object
-  my.corpus <- tm_map(my.corpus, content_transformer(tolower))
-  my.corpus <- tm_map(my.corpus, content_transformer(removeURL))
-  my.corpus <- tm_map(my.corpus, content_transformer(removeSpecial))
-  my.corpus <- tm_map(my.corpus, content_transformer(myRemoveNumbers))
-  my.corpus <- tm_map(my.corpus, content_transformer(mySingleQuote))
-  my.corpus <- tm_map(my.corpus, content_transformer(myRemovePunctuation))
-  my.corpus <- tm_map(my.corpus, content_transformer(myDashApos))
-  # my.corpus <- tm_map(my.corpus, removeWords, stopwords("english"))
-  my.corpus <- tm_map(my.corpus, content_transformer(stripWhitespace))
-  my.corpus <- tm_map(my.corpus, content_transformer(trim))
-  return(my.corpus)
+CleanCorpus <- function(x) {  # input should be a Corpus object
+  x <- tm_map(x, content_transformer(tolower))
+  x <- tm_map(x, content_transformer(removeURL))
+  x <- tm_map(x, content_transformer(convertSpecial))
+  x <- tm_map(x, content_transformer(myRemoveNumbers))
+  x <- tm_map(x, content_transformer(removeProfanity))
+  x <- tm_map(x, content_transformer(myRemovePunct))
+  x <- tm_map(x, content_transformer(myDashApos))
+  # x <- tm_map(x, removeWords, stopwords("english"))
+  x <- tm_map(x, content_transformer(stripWhitespace))
+  x <- tm_map(x, content_transformer(trim))
+  return(x)
 }
 
-## Import training set
-
-
-## Combine into one and clean
+## Import training sets. Combine into one, and clean
 if (!file.exists("./comb.train.txt")) {
   blog.train <- readLines("./blog.train.txt")
   news.train <- readLines("./news.train.txt")
@@ -193,14 +204,15 @@ comb.corpus <- CleanCorpus(comb.corpus.raw)
 rm(comb.train); rm(comb.corpus.raw)
 
 ## Functions to create n-gram Tokenizer to pass on to TDM constructor
+delim <- ' \r\n\t.,;:"()?!'
 BigramTokenizer <- function(x) {
-  NGramTokenizer(x, Weka_control(min=2, max=2))
+  NGramTokenizer(x, Weka_control(min=2, max=2, delimiters=delim))
 }
 TrigramTokenizer <- function(x) {
-  NGramTokenizer(x, Weka_control(min=3, max=3))
+  NGramTokenizer(x, Weka_control(min=3, max=3, delimiters=delim))
 }
 QuadgramTokenizer <- function(x) {
-  NGramTokenizer(x, Weka_control(min=4, max=4))
+  NGramTokenizer(x, Weka_control(min=4, max=4, delimiters=delim))
 }
 ## Functions to construct n-gram TDM
 BigramTDM <- function(x) {
@@ -219,41 +231,31 @@ QuadgramTDM <- function(x) {
 ## From clean corpus, make 1-gram TDM and then dataframe.
 comb.tdm1 <- TermDocumentMatrix(comb.corpus)
 n1 <- data.frame(dimnames(comb.tdm1)$Terms, row_sums(comb.tdm1))
+rm(comb.tdm1)
 colnames(n1) <- c("term", "count")
 rownames(n1) <- NULL
-rm(comb.tdm1)
-write.csv(n1, "n1.csv")
+write.csv(n1, "n1.csv"); rm(n1)  # re-import later
 
-## Remove sparse terms -- doesn't really work because we end up with stopwords
-# max.empty <- 0.99  # set max empty space (zeroes)
-# comb.tdm1.dense <- removeSparseTerms(comb.tdm1, max.empty)
-# comb.tdm1.dense
-
-# For testing and debugging - inspect n-gram TDMs:
-#i <- which(dimnames(blog.tdm1.dense)$Terms == "beer")
-#inspect(blog.tdm1.dense[i+(0:10), 1:20])
-
-## Make n-gram TDMs and turn into dataframe
 comb.tdm2 <- BigramTDM(comb.corpus)
 n2 <- data.frame(dimnames(comb.tdm2)$Terms, row_sums(comb.tdm2))
 colnames(n2) <- c("term", "count")
 rownames(n2) <- NULL
 rm(comb.tdm2)
-write.csv(n2, "n2.csv"); rm(n2)
+write.csv(n2, "n2.csv"); rm(n2)  # re-import later
 
 comb.tdm3 <- TrigramTDM(comb.corpus)
 n3 <- data.frame(dimnames(comb.tdm3)$Terms, row_sums(comb.tdm3))
 colnames(n3) <- c("term", "count")
 rownames(n3) <- NULL
 rm(comb.tdm3)
-write.csv(n3, "n3.csv"); rm(n3)
+write.csv(n3, "n3.csv"); rm(n3)  # re-import later
 
 comb.tdm4 <- QuadgramTDM(comb.corpus)
 n4 <- data.frame(dimnames(comb.tdm4)$Terms, row_sums(comb.tdm4))
 colnames(n4) <- c("term", "count")
 rownames(n4) <- NULL
 rm(comb.tdm4)
-write.csv(n3, "n3.csv"); rm(n3)
+write.csv(n4, "n4.csv"); rm(n4)  # re-import later
 
 ## Function that counts all terms that occur only once
 CountUnk <- function(df) {
@@ -261,17 +263,29 @@ CountUnk <- function(df) {
 }
 
 ## Filter df to get only the terms that appear more than once.
-n1 <- subset(n1, count > 1)
-n1 <- rbind(n1, c("UNK",CountUnk(n1)))
+n1 <- read.csv("n1.csv", stringsAsFactors=F)
+unknowns1 <- CountUnk(n1)
+n1 <- subset(n1[2:3], count > 1)
+n1 <- rbind(n1, c("UNK", unknowns1))
+write.csv(n1, "n1.dense.csv")
 
-n2 <- subset(n2, count > 1)
-n2 <- rbind(n2, c("UNK",CountUnk(n2)))
+n2 <- read.csv("n2.csv", stringsAsFactors=F)
+unknowns2 <- CountUnk(n2)
+n2 <- subset(n2[2:3], count > 1)
+n2 <- rbind(n2, c("UNK", unknowns2))
+write.csv(n2, "n2.dense.csv")
 
-n3 <- subset(n3, count > 1)
-n3 <- rbind(n3, c("UNK",CountUnk(n3)))
+n3 <- read.csv("n3.csv", stringsAsFactors=F)
+unknowns3 <- CountUnk(n3)
+n3 <- subset(n3[2:3], count > 1)
+n3 <- rbind(n3, c("UNK", unknowns3))
+write.csv(n3, "n3.dense.csv")
 
-n4 <- subset(n4, count > 1)
-n4 <- rbind(n4, c("UNK",CountUnk(n4)))
+n4 <- read.csv("n4.csv", stringsAsFactors=F)
+unknowns4 <- CountUnk(n4)
+n4 <- subset(n4[2:3], count > 1)
+n4 <- rbind(n4, c("UNK", unknowns4))
+write.csv(n4, "n4.dense.csv")
 
 ## Function that takes a phrase and looks for it in df
 FindPhrase <- function(x) {  # x must be string
@@ -280,7 +294,6 @@ FindPhrase <- function(x) {  # x must be string
   last3 <- paste(words[len-2], words[len-1], words[len])
   last2 <- paste(words[len-1], words[len])
   last1 <- words[len]
-
 }
 
 
